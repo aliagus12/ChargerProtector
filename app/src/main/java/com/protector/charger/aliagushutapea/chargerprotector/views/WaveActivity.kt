337ff7cfb28a6289de60c72e70d9eb0cd1f4bc8e
Ali Agus Hutapea <aliagushutapea@gmail.com>
2018-12-01 12:30:28 +0700

    -- activity waveView -- 1. tambah activity waveView

---
+++ app/src/main/java/com/protector/charger/aliagushutapea/chargerprotector/views/WaveActivity.kt
@@ -0,0 +1,13 @@
+package com.protector.charger.aliagushutapea.chargerprotector.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.protector.charger.aliagushutapea.chargerprotector.R
+
+class WaveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wave)
+    }
+}
